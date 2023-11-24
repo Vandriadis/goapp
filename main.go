bbc834fa0df20a4b358696aae92ffa80686414fc
Fobb <vandriadis@gmail.com>
2023-11-24 21:10:24 +0200

    Make get req

--- main.go
+++ main.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
-	args := os.Args[1:]
-	if len(args) > 1 {
-		fmt.Printf("Hello world!\nArguments: %v\n", args)
-	} else {
-		fmt.Println("Need args. Example: ./main.go arg1")
+	args := os.Args
+
+	if len(args) < 1 {
+		fmt.Println("Usage: ./main.go <url>")
 		os.Exit(1)
 	}
 
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
 }
